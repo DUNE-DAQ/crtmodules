c42fcc42db8793faaac4fb2f43c8e3ac95e68c35
Eric Flumerfelt <eflumerf@fnal.gov>
2026-01-29 16:01:42 -0600

    Migrate to using DataMoveCallbackRegistry configuration objects for output from Reader modules

--- src/SourceModel.hpp
+++ src/SourceModel.hpp
@@ -54,38 +54,17 @@ public:
   {}
   ~SourceModel() {}
 
-  void set_sink(const std::string& sink_name, bool callback_mode) override
-  {
-    m_callback_mode = callback_mode;
-    if (callback_mode) {
-      TLOG_DEBUG(5) << "Callback mode requested. Won't acquire iom sender!";
-    } else {
-      if (m_sink_is_set) {
-        TLOG_DEBUG(5) << "SourceModel sink is already set in initialized!";
-      } else {
-        m_sink_queue = get_iom_sender<TargetPayloadType>(sink_name);
-        m_sink_is_set = true;
-      }
-    }
-  }
-
   void acquire_callback() override
   {
-    if (m_callback_mode) {
       if (m_callback_is_acquired) {
         TLOG_DEBUG(5) << "SourceModel callback is already acquired!";
       } else {
         // Getting DataMoveCBRegistry
         auto dmcbr = datahandlinglibs::DataMoveCallbackRegistry::get();
-        m_sink_callback = dmcbr->get_callback<TargetPayloadType>(inherited::m_sink_name);
+        m_sink_callback = dmcbr->get_callback<TargetPayloadType>(inherited::m_sink_conf);
         m_callback_is_acquired = true;
       }
-    } else {
-      TLOG_DEBUG(5) << "Won't acquire callback, as IOM sink is set!";
-    }
   }
-
-  std::shared_ptr<sink_t>& get_sink() { return m_sink_queue; }
 
   bool handle_payload(char* message, std::size_t size) // NOLINT(build/unsigned)
   {
@@ -93,17 +72,7 @@ public:
     if (push_out) {
 
       TargetPayloadType& target_payload = *reinterpret_cast<TargetPayloadType*>(message);
-  
-      if (m_callback_mode) {
         (*m_sink_callback)(std::move(target_payload));
-      } else {
-        if (!m_sink_queue->try_send(std::move(target_payload), iomanager::Sender::s_no_block)) {
-          //if(m_dropped_packets == 0 || m_dropped_packets%10000) {
-          //  TLOG() << "Dropped data " << m_dropped_packets;
-          //}
-          ++m_dropped_packets;
-        }
-      }
 
     } else {
       TargetPayloadType target_payload;
@@ -128,13 +97,7 @@ public:
   }
   
 private:
-  // Sink internals
-  std::string m_sink_id;
-  bool m_sink_is_set{ false };
-  std::shared_ptr<sink_t> m_sink_queue;
-
   // Callback internals
-  bool m_callback_mode;
   bool m_callback_is_acquired{ false };
   using sink_cb_t = std::shared_ptr<std::function<void(TargetPayloadType&&)>>;
   sink_cb_t m_sink_callback;
