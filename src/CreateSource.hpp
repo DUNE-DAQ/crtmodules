a5530d1a2fc05c959476fb7022761e19650ae3ce
Eric Flumerfelt <eflumerf@fnal.gov>
2026-02-05 07:49:11 -0600

    Rename RawDataCallback* -> DataMoveCallback*

--- src/CreateSource.hpp
+++ src/CreateSource.hpp
@@ -26,7 +26,7 @@ DUNE_DAQ_TYPESTRING(dunedaq::fdreadoutlibs::types::CRTGrenobleTypeAdapter, "CRTG
 namespace crtmodules {
 
 std::shared_ptr<SourceConcept>
-createSourceModel(const appmodel::RawDataCallbackConf* conf)
+createSourceModel(const appmodel::DataMoveCallbackConf* conf)
 {
   auto datatype = conf->get_data_type();
   TLOG() << "Choosing specializations for SourceModel for output connection "
